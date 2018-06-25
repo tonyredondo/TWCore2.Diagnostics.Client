701da08edf8c86f595fc57a98c9a37b90b2d4720
daniel.redondo <daniel.redondo@agsw.es>
2018-06-25 09:47:39 +0200

    components.

--- src/app/views/diagnostics/logs.component.ts
+++ src/app/views/diagnostics/logs.component.ts
@@ -7,12 +7,10 @@ import { BsDatepickerConfig, BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { defineLocale } from 'ngx-bootstrap/chronos';
 import { enGbLocale } from 'ngx-bootstrap/locale';
 defineLocale('en-gb', enGbLocale);
-
 import { moment } from 'ngx-bootstrap/chronos/test/chain';
 import { LogLevelEnum } from '../../services/api/model/loglevel';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { BaseChartDirective } from 'ng2-charts/ng2-charts';
-
 // Charts
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
--- src/app/views/diagnostics/traces.component.ts
+++ src/app/views/diagnostics/traces.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { QueryService } from '../../services/api/api/query.service';
+import { environment } from '../../../environments/environment';
+import { ApplicationsLevels, PagedListNodeLogItem, SerializableException, LogSummary } from '../../services/api';
+import { Observable } from 'rxjs/Observable';
+import { BsDatepickerConfig, BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { enGbLocale } from 'ngx-bootstrap/locale';
+defineLocale('en-gb', enGbLocale);
+import { moment } from 'ngx-bootstrap/chronos/test/chain';
+import { LogLevelEnum } from '../../services/api/model/loglevel';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+// Charts
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+
 
 @Component({
   templateUrl: 'traces.component.html'
