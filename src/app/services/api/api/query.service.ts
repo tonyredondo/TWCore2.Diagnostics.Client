4d138ae487148a630c0532d270beecdaeb8c8607
daniel.redondo <daniel.redondo@agsw.es>
2018-06-27 16:43:10 +0200

    changes.

--- src/app/services/api/api/query.service.ts
+++ src/app/services/api/api/query.service.ts
@@ -20,6 +20,7 @@ import { Observable }                                        from 'rxjs/Observab
 
 import { LogSummary } from '../model/logSummary';
 import { NodeStatusItem } from '../model/nodeStatusItem';
+import { NodeTraceItem } from '../model/nodeTraceItem';
 import { PagedListNodeLogItem } from '../model/pagedListNodeLogItem';
 import { PagedListNodeStatusItem } from '../model/pagedListNodeStatusItem';
 import { PagedListNodeTraceItem } from '../model/pagedListNodeTraceItem';
@@ -548,6 +549,58 @@ export class QueryService {
         return this.httpClient.get<PagedListNodeStatusItem>(`${this.basePath}/api/query/${encodeURIComponent(String(environment))}/status`,
             {
                 params: queryParameters,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     *
+     *
+     * @param environment
+     * @param groupName
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public apiQueryByEnvironmentTracesByGroupNameGet(environment: string, groupName: string, observe?: 'body', reportProgress?: boolean): Observable<Array<NodeTraceItem>>;
+    public apiQueryByEnvironmentTracesByGroupNameGet(environment: string, groupName: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<NodeTraceItem>>>;
+    public apiQueryByEnvironmentTracesByGroupNameGet(environment: string, groupName: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<NodeTraceItem>>>;
+    public apiQueryByEnvironmentTracesByGroupNameGet(environment: string, groupName: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+        if (environment === null || environment === undefined) {
+            throw new Error('Required parameter environment was null or undefined when calling apiQueryByEnvironmentTracesByGroupNameGet.');
+        }
+        if (groupName === null || groupName === undefined) {
+            throw new Error('Required parameter groupName was null or undefined when calling apiQueryByEnvironmentTracesByGroupNameGet.');
+        }
+
+        let headers = this.defaultHeaders;
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'text/plain',
+            'application/json',
+            'text/json',
+            'application/xml',
+            'text/xml',
+            'application/binary-formatter',
+            'application/n-binary',
+            'application/pw-binary',
+            'application/w-binary'
+        ];
+        let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set("Accept", httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        let consumes: string[] = [
+        ];
+
+        return this.httpClient.get<Array<NodeTraceItem>>(`${this.basePath}/api/query/${encodeURIComponent(String(environment))}/traces/${encodeURIComponent(String(groupName))}`,
+            {
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
                 observe: observe,
--- src/app/views/diagnostics/tracedetails.component.ts
+++ src/app/views/diagnostics/tracedetails.component.ts
@@ -1,5 +1,8 @@
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { QueryService } from '../../services/api/api/query.service';
+import { environment } from '../../../environments/environment';
+import { NodeTraceItem } from '../../services/api';
 
 @Component({
   templateUrl: 'tracedetails.component.html'
@@ -7,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
 export class TraceDetailsComponent implements OnInit {
   private _params: Params;
   private _queryParams: Params;
-  constructor(private _activatedRoute: ActivatedRoute, private _router: Router) { }
+  public group: string = null;
+  public items: INodeTraceItemExt[] = [];
+  constructor(private _queryService: QueryService, private _activatedRoute: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
     this._params = Object.assign({}, this._activatedRoute.snapshot.params);
     this._queryParams = Object.assign({}, this._activatedRoute.snapshot.queryParams);
+    if (this._params.group !== undefined) {
+      this.group = this._params.group;
+      this.updateData();
+    }
   }
+  updateData() {
+    this._queryService.apiQueryByEnvironmentTracesByGroupNameGet(environment.name, this.group).subscribe(lstTraces => {
+      this.items.length = 0;
+      for (let i = 0; i < lstTraces.length; i++) {
+        const item = lstTraces[i];
+        const itemTags = item.tags.split(', ');
+        const tags = [] as TagItem[];
+        for (let it = 0; it < itemTags.length; it++) {
+          const itemTagItem = itemTags[it].split(': ');
+          tags.push({ key: itemTagItem[0], value: itemTagItem[1] });
+        }
+        this.items.push(Object.assign(item, { tagsArray : tags }));
+      }
+    });
+  }
+}
+
+interface INodeTraceItemExt extends NodeTraceItem {
+  tagsArray: TagItem[];
+}
+interface TagItem {
+  key: string;
+  value: string;
 }
