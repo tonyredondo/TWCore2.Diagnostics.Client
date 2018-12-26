7c5279abd2286377cf2f44c2e2b6f073ab040fef
Daniel Redondo <tonyredondo@gmail.com>
2018-12-26 12:58:02 +0100

    .

--- src/app/views/diagnostics/status.component.ts
+++ src/app/views/diagnostics/status.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 export class StatusComponent implements OnInit {
   private _params: Params;
   private _queryParams: Params;
+
+  // barChart
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = false;
+
+  public barChartData: any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
   constructor(private _activatedRoute: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
